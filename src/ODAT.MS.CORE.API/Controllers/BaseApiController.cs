a45ec7e541feb3716068cfb1aadb8c762045f6b7
Chaminda Chandrasekara <chaminda_chandrasekara@yahoo.com>
2021-10-17 11:40:08 +0000

    Base entity and controller

---
+++ src/ODAT.MS.CORE.API/Controllers/BaseApiController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Query;
+using Microsoft.AspNetCore.OData.Routing.Controllers;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using ODAT.MS.CORE.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ODAT.MS.CORE.API.Controllers
+{
+    class BaseApiController<T, S> : ODataController where T : BaseEntity<T> where S : ODataController
+    {
+        protected readonly DbContext _DbContext;
+        protected readonly ILogger<S> _logger;
+        private readonly DbSet<T> dbSet;
+        public BaseApiController(ILogger<S> logger, DbContext DbContext)
+        {
+            _logger = logger;
+            _DbContext = DbContext;
+            dbSet = _DbContext.Set<T>();
+        }
+
+        [EnableQuery]
+        public virtual IActionResult Get()
+        {
+            return Ok(_DbContext.Set<T>().AsQueryable<T>());
+        }
+
+        public virtual IActionResult Post([FromBody] T entity)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            dbSet.Add(entity);
+            _DbContext.SaveChanges();
+            return Created(entity);
+        }
+
+
+        public virtual IActionResult Put(int key, [FromBody] T entity)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            dbSet.Update(entity);
+            _DbContext.SaveChanges();
+            return Ok(entity);
+        }
+
+        public virtual IActionResult Delete(int key)
+        {
+            dbSet.Remove(dbSet.Find(key));
+            _DbContext.SaveChanges();
+            return NoContent();
+        }
+    }
+}
---
+++ src/ODAT.MS.CORE.API/Models/BaseEntity.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ODAT.MS.CORE.API.Models
+{
+    public class BaseEntity<T> : IEntityTypeConfiguration<T> where T : BaseEntity<T>
+    {
+
+        //ObjVersion
+        [ConcurrencyCheck]
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public uint xmin { get; set; }
+
+        public void Configure(EntityTypeBuilder<T> builder)
+        {
+            //builder.UseXminAsConcurrencyToken();
+        }
+    }
+}
