0031e81e59fe6a3ebd38d8d42d48c09d4fbba962
Chaminda Chandrasekara <chaminda_chandrasekara@yahoo.com>
2021-10-25 05:42:22 +0000

    ark as primary key

--- src/ODATA.MS.CORE.API/Models/BaseEntity.cs
+++ src/ODATA.MS.CORE.API/Models/BaseEntity.cs
@@ -12,6 +12,7 @@ namespace ODATA.MS.CORE.API.Models
 {
     public class BaseEntity<T> : IEntityTypeConfiguration<T> where T : BaseEntity<T>
     {
+        [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
